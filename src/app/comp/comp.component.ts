36ed8b0f7b9aa7b8d40febdfee6a28b05c1e13b9
ViktorijaProjkova <projkova.viktorija@gmail.com>
2024-12-25 23:25:44 +0100

    домашна-Користење на ARIA attributes во Angular

--- src/app/comp/comp.component.ts
+++ src/app/comp/comp.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { driver } from '../../driver';
+import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { style } from '@angular/animations';
 @Component({
   selector: 'app-comp',
-  imports: [],
+  imports:[CommonModule],
   templateUrl: './comp.component.html',
   styleUrl: './comp.component.css'
 })
@@ -13,7 +16,33 @@ export class CompComponent {
   indeks:number| undefined;
 
   onDrvView(){
-    console.log("Klikna me!")
+    //console.log("Klikna me!")
+    let link: string |undefined; //ova e TS
+    //var link=''; ako sakame JS
+    if(this.vozac?.iconUrl)
+    {link=this.vozac?.iconUrl}
+    else{
+      link="https://www.google.com/"
+    };
+    window.open(link,"_blank")
   }
+  klasi()//ova e isto so klasi2 samo sto e vo jquery
+  //tuka e postrogo i ako ne odgovara so izrazot ne nasleduva klasa na pr lunatic kje postane neizboeno
+  {
+    return {'begin':this.vozac?.category=='ASD',
+      'adv':this.vozac?.category=='EXPERT',
+      'undr':true}
+    }
 
+  klasi2(){ //ova e bukv istoto samo vo java sccript 
+    //ovde sekako se nasleduva nekoja klasa
+    if(this.vozac?.category=='ASD'){return 'begin'}
+    else{return 'adv'}
+  }
+  //ANGULAR ZNAE DA GI RAZVIVA I DVETE 
+  stilovi(){
+    if(this.vozac?.category=='EXPERT'){return 'underline'}
+    else{return 'none'}
+  }
 }
+
